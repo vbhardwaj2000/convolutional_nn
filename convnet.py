added training=true and fixed batch normalization. Also added visualization weights file

convnet.py
@@ -27,12 +27,16 @@ tf.summary.image("input_image", input_placeholder)
 
 # Normalize image
 # Subtract off the mean and divide by the variance of the pixels.
-normalized_image = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), input_placeholder)
+
+random_image = tf.map_fn(lambda frame: tf.image.random_flip_left_right(frame),input_placeholder)
+tf.size(random_image, name=None, out_type=tf.int32)
+#tf.size(random_image,dtypes.int32)
+normalized_image = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), random_image)
 
 # TODO add conv layers here
 
 conv_layer_1 = tf.layers.conv2d(normalized_image,
-                                filters=42,
+                                filters=32,
                                 kernel_size=(3, 3),
                                 strides=(1, 1),
                                 padding='same',
@@ -40,21 +44,83 @@ conv_layer_1 = tf.layers.conv2d(normalized_image,
 
 conv_layer_1_with_bn = tf.layers.batch_normalization(conv_layer_1, training=True)
 
+
+conv_layer_2 = tf.layers.conv2d(conv_layer_1_with_bn,
+                                filters=32,
+                                kernel_size=(3, 3),
+                                strides=(1, 1),
+                                padding='same',
+                                activation=tf.nn.relu)
+
+conv_layer_2_with_bn = tf.layers.batch_normalization(conv_layer_2, training=True)
 # final_conv_layer = normalized_image # change me
 
-pool_layer_1 = tf.layers.max_pooling2d(conv_layer_1_with_bn,
+pool_layer_1 = tf.layers.max_pooling2d(conv_layer_2_with_bn,
                                        pool_size=(2, 2),
                                        strides=(2, 2))
 
+conv_layer_3 = tf.layers.conv2d(pool_layer_1,
+                                filters=64,
+                                kernel_size=(3, 3),
+                                strides=(1, 1),
+                                padding='same',
+                                activation=tf.nn.relu)
+
+conv_layer_3_with_bn = tf.layers.batch_normalization(conv_layer_3, training=True)
+
+
+conv_layer_4 = tf.layers.conv2d(conv_layer_3_with_bn,
+                                filters=64,
+                                kernel_size=(3, 3),
+                                strides=(1, 1),
+                                padding='same',
+                                activation=tf.nn.relu)
+
+conv_layer_4_with_bn = tf.layers.batch_normalization(conv_layer_4, training=True)
+
+pool_layer_2 = tf.layers.max_pooling2d(conv_layer_4_with_bn,
+                                       pool_size=(2, 2),
+                                       strides=(2, 2))
+
+
+#conv_layer_5 = tf.layers.conv2d(pool_layer_2,
+#                                filters=128,
+#                                kernel_size=(3, 3),
+#                                strides=(1, 1),
+#                                padding='same',
+#                                activation=tf.nn.relu)
+
+#conv_layer_5_with_bn = tf.layers.batch_normalization(conv_layer_5, training=True)
+
+
+#conv_layer_6 = tf.layers.conv2d(conv_layer_5_with_bn,
+#                                filters=128,
+#                                kernel_size=(3, 3),
+#                                strides=(1, 1),
+#                                padding='same',
+#                                activation=tf.nn.relu)
+
+#conv_layer_6_with_bn = tf.layers.batch_normalization(conv_layer_6, training=True)
+
+#pool_layer_3 = tf.layers.max_pooling2d(conv_layer_6_with_bn,
+ #                                      pool_size=(4, 4),
+  #                                     strides=(4, 4))
+
+
+
+
 # convert 3d image to 1d tensor (don't change batch dimension)
-flat_tensor = tf.contrib.layers.flatten(pool_layer_1)
+flat_tensor = tf.contrib.layers.flatten(pool_layer_2)
 
 # TODO improve fully connected layers
 ## Neural network hidden layers
-hidden_layer_1 = tf.layers.dense(flat_tensor, 10, activation=tf.nn.relu)
+hidden_layer_1 = tf.layers.dense(flat_tensor, 160, activation=tf.nn.relu)
+hidden_layer_1_with_bn = tf.layers.batch_normalization(hidden_layer_1, training=True)
+hidden_layer_2 = tf.layers.dense(hidden_layer_1_with_bn, 60, activation=tf.nn.relu)
+hidden_layer_2_with_bn = tf.layers.batch_normalization(hidden_layer_2, training=True)
 
 ## Logit layer
-logits = tf.layers.dense(hidden_layer_1, 10)
+logits = tf.layers.dense(hidden_layer_1_with_bn, 10)
 
 # label placeholder
 label_placeholder = tf.placeholder(tf.uint8, shape=[None])
@@ -93,7 +159,7 @@ with tf.Session() as sess:
         step_count += 1
 
         # get batch of training data
-        batch_training_data, batch_training_labels = dataUtils.getCIFAR10Batch(is_eval=False, batch_size=300)
+        batch_training_data, batch_training_labels = dataUtils.getCIFAR10Batch(is_eval=False, batch_size=100)
 
         # train network
         training_accuracy, training_loss, summary, _ = sess.run([accuracy, loss, summary_tensor, train],
@@ -106,7 +172,7 @@ with tf.Session() as sess:
         # every 10 steps check accuracy
         if step_count % 10 == 0:
             # get Batch of test data
-            batch_test_data, batch_test_labels = dataUtils.getCIFAR10Batch(is_eval=True, batch_size=300)
+            batch_test_data, batch_test_labels = dataUtils.getCIFAR10Batch(is_eval=True, batch_size=100)
 
             # do eval step to test accuracy
             test_accuracy, test_loss, summary = sess.run([accuracy, loss, summary_tensor],